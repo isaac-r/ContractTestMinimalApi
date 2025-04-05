7a08ed300a34b2af24ca7526e00c95bc20c7e2c9
isaac.richardson <isaacr828@gmail.com>
2025-04-05 09:00:49 +0100

    add main and createApp methods

--- ContractTestMinimalApi.Api/Program.cs
+++ ContractTestMinimalApi.Api/Program.cs
@@ -1,32 +1,51 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddAuthorization();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace ContractTestingApi
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapGet("/api/getExpenseById/{id:int}", (int id) =>
-{
-    return Results.Ok(new
+    public static class Program
     {
-        Id = id,
-        Name = "Test Expense",
-        Amount = 100.00,
-    });
-});
+        public static void Main(string[] args)
+        {
+            var app = CreateApp(args);
+            app.Run();
+        }
 
-app.Run();
+        public static WebApplication CreateApp(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            ConfigureServices(builder.Services);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapGet("/api/getExpenseById/{id:int}", (int id) =>
+            {
+                return Results.Ok(new
+                {
+                    Id = id,
+                    Name = "Test Expense",
+                    Amount = 100.00,
+                });
+            });
+
+            return app;
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddAuthorization();
+        }
+    }
+}
