1217105e27cd1f45764e65d13949e8668c80a2df
isaac.richardson <isaacr828@gmail.com>
2025-04-05 09:01:05 +0100

    add provider pact test

---
+++ ContractTestMinimalApi.Test/ContractTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using ContractTestingApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractTestMinimalApi.Test
+{
+    public class ContractTestFixture
+    {
+        public async Task InitializeAsync()
+        {
+            var _app = Program.CreateApp(new string[] { });
+            _app.Urls.Clear();
+            _app.Urls.Add("https://localhost:5000");
+            await _app.StartAsync();
+        }
+    }
+}
---
+++ ContractTestMinimalApi.Test/ProviderTest.cs
@@ -0,0 +1,49 @@
+﻿using PactNet.Verifier;
+using PactNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using PactNet.Output.Xunit;
+using PactNet.Infrastructure.Outputters;
+
+namespace ContractTestMinimalApi.Test
+{
+    public class ProviderTest : IClassFixture<ContractTestFixture>
+    {
+        private readonly string _directory = "../../../../pacts";
+        private readonly ContractTestFixture _fixture;
+        private readonly ITestOutputHelper _output;
+
+        public ProviderTest(ContractTestFixture fixture, ITestOutputHelper output)
+        {
+            _fixture = fixture;
+            _output = output;
+        }
+
+        [Fact]
+        [Trait("Category", "Provider")]
+        public async Task GivenValidRequest_WhenGetExpenseById_ThenExpectedResponse()
+        {
+            await _fixture.InitializeAsync();
+
+            var pactFile = $"{_directory}/ExpenseConsumer-ExpenseProvider.json";
+
+            var config = new PactVerifierConfig
+            {
+                Outputters = new List<IOutput>
+                {
+                    new XunitOutput(_output)
+                },
+                LogLevel = PactLogLevel.Information
+            };
+
+            new PactVerifier("ExpenseProvider", config)
+                .WithHttpEndpoint(new Uri("https://localhost:5000"))
+                .WithFileSource(new FileInfo(pactFile))
+                .Verify();
+        }
+    }
+}
